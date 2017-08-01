649316d0284854440c7b5dc408a23cdc6c8bb5a9
Cy Chan <cychan@lbl.gov>
2017-08-01 12:58:10 -0700

    update hpgmg xmls

--- examples/hpgmg/genxml.sh
+++ examples/hpgmg/genxml.sh
@@ -7,8 +7,10 @@ EXE="${EXASAT_TOP}/src/genCodeDescript"
 : ${HPGMG_TOP="${ORIG_DIR}/../../../lbl/hpgmg"}
 cd ${HPGMG_TOP}/finite-volume/source
 
+OUT_DIR=${ORIG_DIR}/xml
+
 for op_type in 7pt 27pt fv2 fv4; do
-  ${EXE} -I./ -DUSE_GSRB -DGSRB_BRANCH  -DUSE_HELMHOLTZ hpgmg-fv.c mg.c operators.${op_type}.c | tee ${ORIG_DIR}/hpgmg.${op_type}.br.xml
-  ${EXE} -I./ -DUSE_GSRB -DGSRB_FP      -DUSE_HELMHOLTZ hpgmg-fv.c mg.c operators.${op_type}.c | tee ${ORIG_DIR}/hpgmg.${op_type}.fp.xml
-  ${EXE} -I./ -DUSE_GSRB -DGSRB_STRIDE2 -DUSE_HELMHOLTZ hpgmg-fv.c mg.c operators.${op_type}.c | tee ${ORIG_DIR}/hpgmg.${op_type}.s2.xml
+  ${EXE} -I./ -DUSE_GSRB -DGSRB_BRANCH  -DUSE_HELMHOLTZ hpgmg-fv.c mg.c operators.${op_type}.c | tee ${OUT_DIR}/hpgmg.${op_type}.br.xml
+  ${EXE} -I./ -DUSE_GSRB -DGSRB_FP      -DUSE_HELMHOLTZ hpgmg-fv.c mg.c operators.${op_type}.c | tee ${OUT_DIR}/hpgmg.${op_type}.fp.xml
+  ${EXE} -I./ -DUSE_GSRB -DGSRB_STRIDE2 -DUSE_HELMHOLTZ hpgmg-fv.c mg.c operators.${op_type}.c | tee ${OUT_DIR}/hpgmg.${op_type}.s2.xml
 done
